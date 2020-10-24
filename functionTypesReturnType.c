38223539c2f4a826fb37e5cdb5ccc88cb6fd9f7a
Giovanni Rodriguez G <34606945+grg8881m@users.noreply.github.com>
2020-10-23 20:08:09 -0500

    Add files via upload

---
+++ functionTypesReturnType.c
@@ -0,0 +1,29 @@
+Funciones sin argumentos y sin valor de retorno
+
+void functionName();
+
+functionName();
+
+void functionName()
+{
+    //logica
+}
+
+Funciones con argumentos pero sin valores a Retornar
+
+void functionName(float);
+
+functionName(float);
+
+void functionName(float)
+{
+    //logica
+}
+
+Funciones sin argumentos pero que si tienen valores a retornar
+
+float functionName();
+
+Funciones con argumentos con return value
+
+float functionName(int, float);
